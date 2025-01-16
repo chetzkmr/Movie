3178daa0a4536c71e18211b40e38712a0699c8ac
Chethan <134191207+chetzkmr@users.noreply.github.com>
2025-01-16 09:08:46 +0530

    Update movie.py

--- movie.py
+++ movie.py
@@ -3,8 +3,10 @@ import streamlit as st
 import pickle
 import gzip
 
+api_key= st.secrets["api_key"]
+
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    url =f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US"
     data = requests.get(url).json()
     poster_path = data['poster_path']
     full_path = "http://image.tmdb.org/t/p/w500/" + poster_path
